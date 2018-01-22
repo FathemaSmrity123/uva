000c5a86631f5a6f0a102c38ee29272d352a9d63
FathemaSmrity123 <ummehabibaprity003@gmail.com>
2018-01-22 07:01:26 +0000

    using 2d greed

--- uva minesweeper.cpp
+++ uva minesweeper.cpp
@@ -38,7 +38,7 @@ int main()
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
-            {
+            
                 if(a[i][j]!='*')
                     b[i][j]=0;
                 else
